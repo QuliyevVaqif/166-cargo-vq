1fb8ebcacaf703c2408fbd1674c47f99ed21f3a1
QuliyevVaqif <vaqifq366@gmail.com>
2025-09-18 19:16:17 +0400

    Added some JavaScript

--- 166.az/assets/js/index.js
+++ 166.az/assets/js/index.js
@@ -2,14 +2,43 @@ document.querySelectorAll(".faqTitle").forEach((btn) => {
   btn.addEventListener("click", () => {
     const body = btn.nextElementSibling;
 
+    btn.classList.toggle("open");
     body.classList.toggle("active");
+
+    document.querySelectorAll(".faqBody").forEach((check) => {
+      if (body !== check) {
+        check.classList.remove("active");
+        check.previousElementSibling.classList.remove("open");
+      }
+    });
   });
 });
 
-document.querySelectorAll(".faqTitle").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const body = btn.nextElementSibling;
+document.querySelectorAll(".faqKateqoriyalar li").forEach((listElement) => {
+  listElement.addEventListener("click", () => {
+    listElement.classList.add("changeBorder");
 
-    body.classList.toggle("active");
+    const btn = listElement.firstElementChild;
+
+    btn.classList.add("change-color");
+
+    document.querySelectorAll(".faqKateqoriyalar li").forEach((check) => {
+      if (check !== listElement) {
+        check.classList.remove("changeBorder");
+        check.firstElementChild.classList.remove("change-color");
+      }
+    });
+  });
+});
+
+document.querySelectorAll(".musteri-option div").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.add("musteri-color");
+
+    document.querySelectorAll(".musteri-option div").forEach((check) => {
+      if (btn !== check) {
+        check.classList.remove("musteri-color");
+      }
+    });
   });
 });
